e68626abb350d0398425e6046a3b77e284846fae
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-03 02:01:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast  = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.append("Odie");
+append.push("Odie");
 prepend.prepend("Odie");
